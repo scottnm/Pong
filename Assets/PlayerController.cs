b1ecb5a4e6b9ffafcdb9a8fc42d2a1dba0981073
Darwin <darwinpek@gmail.com>
2014-02-02 02:02:26 -0600

    Add player controls

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public enum Player { Player1, Player2 };
+	public Player player;
+	
+	private float speed = 6f;
+	
+	void FixedUpdate () {
+		string playerString = player == Player.Player1 ? "P1" : "P2";
+		float direction = Input.GetAxis("Vertical-" + playerString);
+		rigidbody2D.velocity = new Vector2(0, speed * direction);
+	}
+}
