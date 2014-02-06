c5ccb803bbf75973c5aa9ed2d512316a42edbf0e
William Vickery <william@vickery.com>
2014-02-06 14:30:47 -0600

    added some animations

---
+++ Assets/Scripts/WallController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallController : MonoBehaviour {
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+	    animator = gameObject.GetComponent<Animator>();
+	}
+	
+    void OnTriggerEnter2D(Collider2D coll) {
+        Debug.Log("I was hit.");
+        animator.SetBool("Hit", true);
+    }
+    void OnTriggerExit2D(Collider2D coll) {
+        Debug.Log("it left");
+        animator.SetBool("Hit", false);
+    }
+}
